77b222d00b033319e35c480683354768f9fb2ec7
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-04-17 09:19:32 +0100

    executed my first hello world javascript program outside the browser

---
+++ Hello World/index.js
@@ -0,0 +1,15 @@
+/** 
+ * Executing Javascript with Node
+ * 1. Node REPL
+ * Read
+ * Evaluate
+ * Print
+ * Loop
+ * 
+ * 2.Executing code in Javascript file in the command line
+ * node filename
+ * e.g node index.js or node index
+ * This executes the code in the file in the command line
+ * **/
+
+console.log("Hello from index.js");
