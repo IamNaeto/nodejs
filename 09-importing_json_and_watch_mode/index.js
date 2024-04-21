57cd19b36b01972fe2fb3c0482ee5349147388a7
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2024-04-21 10:51:22 +0100

    importing json and watch mode in nodejs

---
+++ 09-importing_json_and_watch_mode/index.js
@@ -0,0 +1,9 @@
+const data = require("./data");
+console.log(data);
+console.log(data.name);
+console.log(data.address);
+console.log(data.address.street);
+console.log(data.address.city);
+
+console.log("Watch mode activated yooooooooooo!")
+console.log("Am loving this watch mode, yeeeeeeee!")
